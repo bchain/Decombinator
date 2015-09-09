9b1887c55dc71dcbae636fc3ac15130cfa861c2c
unknown <regfbec@C00014675.ad.ucl.ac.uk>
2015-09-09 17:40:37 +0100

    Corrected decombinatedm batch file so as to collect the barcode in teh right place

--- decombinate.bat
+++ decombinate.bat
@@ -7,4 +7,4 @@ REM note the Decombinator commands
 REM -i is input file -o is output folder
 REM -b True means extract barcode from bs1 to be1; and from bs2 to be2. Note that first base pair is 0, NOT 1
 
-FOR /f %%A IN ('DIR /B *.fastq')DO decombinatorV2_2.py -i %%A  -o %%A -b True -bs1 0 -be1 28 -bs2 28 -be2 28 -p False -c True -of True -f True
+FOR /f %%A IN ('DIR /B *.fastq')DO decombinatorV2_2.py -i %%A  -o %%A -b True -bs1 250 -be1 278 -bs2 278 -be2 278 -p False -c True -of True -f True
--- decombinatem.bat
+++ decombinatem.bat
@@ -6,5 +6,5 @@ REM cycle though this file and DECOMBINATE
 REM note the Decombinator commands
 REM -i is input file -o is output folder
 REM -b True means extract barcode from bs1 to be1; and from bs2 to be2. Note that first base pair is 0, NOT 1
-
+REM barcodes are at the end of the read. e.g. 250 : 278; or 250 :256
 FOR /f %%A IN ('DIR /B *.fastq')DO decombinatorV2_2.py -i %%A  -o %%A -b True -bs1 0 -be1 6 -bs2 6 -be2 6 -p True -c True -of True -f True -s mouse
